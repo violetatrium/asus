7a61181a18e6863823eae1e4542e01c657d6e291
venkata <venkat.chimata@gryphonconnect.com>
2018-12-07 00:36:38 +0530

    Updated build.sh script to be in sync with the format of other routers

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 MODEL=$1
 rm -f release/src/router/unum/unum.tgz
-if [ "$1" == "--add" ]; then
-  UNUM="$2"
+if [ "$2" == "--add" ]; then
+  UNUM="$3"
   if [ ! -f "$UNUM" ]; then
     echo "Unable to find \"$UNUM\"."
     echo "Please specify full path to the file."
     exit 1
   fi
-  MODEL="$3"
+  MODEL="$1"
   cp $UNUM release/src/router/unum/unum.tgz
   echo "Building firmware with $UNUM..."
 elif [ "$1" == "--no-changes" ]; then
@@ -15,8 +15,8 @@ elif [ "$1" == "--no-changes" ]; then
   echo "Building firmware without changes."
   MODEL="$2"
 else
-  echo "Usage: $0 <--add <path_to_unum.tgz> <Asus_HW_MODEL> |--no-changes> <Asus_HW_MODEL>"
-  echo "Build the Readynet based firmware with UNUM additons (--add option)"
+  echo "Usage: $0 <Asus_HW_MODEL> <--add <path_to_unum.tgz> |--no-changes> <Asus_HW_MODEL>"
+  echo "Build the Asus based firmware with UNUM additons (--add option)"
   echo "or without them (--no-changes) option."
   exit 2
 fi
@@ -29,6 +29,9 @@ else
 	exit 1
 fi
 mydir=`pwd`
+
+echo $UNUM
+echo $MODEL
 
 export PATH=/opt/openwrt-gcc463.mips.mesh/bin:$PATH
 export STAGING_DIR=/opt/openwrt-gcc463.mips
